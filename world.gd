bcef7af01ba51ebc981713bb986c52cabe61f7e5
Pascal <hschmidtopascal@gmail.com>
2023-12-30 18:24:31 +0100

    added script for global variables, added gameworld script and changed enemy, player and world layout

---
+++ world.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var battle_node = preload("res://battlescreen.tscn")
+var current_scene = "world"
+var change_scene = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
