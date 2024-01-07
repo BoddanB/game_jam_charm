50cb69838dc9af9496fe4533a6421d0b04bfbbdc
Pascal <hschmidtopascal@gmail.com>
2024-01-07 14:02:49 +0100

    added a sprite flip depending on player position added scene switch if area is entered

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -1,10 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 30.0
+const MAX_SPEED = 25.0
 const JUMP_VELOCITY = -400.0
 
-var follow_player = false
 var player = null
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -19,15 +18,16 @@ func _physics_process(delta):
 
 	move_and_slide()
 
-func flip_animation(playerPosition: float):
-	if playerPosition < 0:
-		pass #Flip the animation to the left.
+func flip_animation(enemyPosition: float):
+	if enemyPosition <= 0:
+		$Sprite2D.flip_h = true
 	else:
-		pass #Flip the animation to the right.
+		$Sprite2D.flip_h = false
 
 func chase():
-	if follow_player:
-		position += (player.position - position) / SPEED
+	if player:
+		position.x += (player.position.x - position.x) / MAX_SPEED
+		flip_animation(player.position.x - position.x)
 		#To play the animation add it here.
 	else:
 		pass #Play an idle animation or something similar.
@@ -36,10 +36,12 @@ func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-func _on_detection_area_body_exited(body:Node2D):
+func _on_detection_area_body_exited(_body:Node):
 	player = null
-	follow_player = false
 
 func _on_detection_area_body_entered(body:Node2D):
 	player = body
-	follow_player = true
+
+
+func _on_enemyhitbox_body_entered(_body:Node):
+	Global.change_scene("res://battlescreen.tscn")
