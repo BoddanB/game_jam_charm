08168fafb800f310f6167da36630ab0f96b7ea4e
Pascal <hschmidtopascal@gmail.com>
2024-01-07 13:56:18 +0100

    Changed the spawn positions and resized the platforms

--- battlescreen.gd
+++ battlescreen.gd
@@ -1,16 +1,15 @@
 extends Node2D
 
-var player_node = preload("res://Player/player.tscn")
-var enemy_node = preload("res://enemy/enemy.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var player = player_node.instance()
-	player.position = Vector2(143, 167)
+	var player = Global.player_node.instantiate()
+	player.position = Vector2(166, 167)
 	add_child(player)
-	var enemy = enemy_node.instance()
-	enemy.position = Vector2(183, 167)
-	add_child(enemy)
+	var enemy = Global.enemy_node.instantiate()
+	enemy.position = Vector2(184, 167)
+	call_deferred("add_child", enemy)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
